82271c5dc2074fcbed3fa982882aa9baf05a7a0e
Letícia Farias <40062831+LeticiaFarias@users.noreply.github.com>
2019-03-15 16:54:17 -0300

    Atualizando questão 5

--- primos.c
+++ primos.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
-int primo(int n) {
-	if (n % ){
+int primo (int n, int aux) {
+	if ((n % n == 0) && (n != 2)){
+		return 0;
+	} else if (aux > (n / 2)) {
+		return 1;
+	} else {
+		return primo(n, aux + 1);
+	}
+	
+}
+
+int main () {
+	setlocale(LC_ALL, "Portuguese");
+	int n;
+	printf ("Digite o número: ");
+	scanf ("%d", &n);
+	
+	if (primo(n, 1)) {
+	    	printf ("1");
+	} else {
+		printf ("0");
 	}
 }
-
-int main() {
-	
-	
-	return 0;
-}
-
